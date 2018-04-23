fe264068b9c835ece203cda47917fa083c6eddc0
Wouter Toering <wouter.toering@gmail.com>
2018-04-23 22:32:33 +0200

    Serialize everything

--- apps/workout_tracker/serializers.py
+++ apps/workout_tracker/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 
-from apps.workout_tracker.models import LOG_STATUS_CHOICES, WorkoutLog
+from apps.workout_tracker.models import (
+    LOG_STATUS_CHOICES,
+    Exercise,
+    ExerciseType,
+    Workout,
+    WorkoutLog
+)
 
 
 class ChoiceField(serializers.ChoiceField):
@@ -17,12 +23,38 @@ class ChoiceField(serializers.ChoiceField):
         self.fail('invalid_choice', input=data)
 
 
+class ExerciseTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ExerciseType
+        fields = ('name', 'weight_progression')
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    exercise_type = ExerciseTypeSerializer(read_only=True)
+
+    class Meta:
+        model = Exercise
+        fields = ('sets', 'reps', 'is_amrap', 'exercise_type')
+
+
+class WorkoutSerializer(serializers.ModelSerializer):
+    exercises = ExerciseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Workout
+
+        fields = (
+            'exercises',
+        )
+
+
 class WorkoutLogsSerializer(serializers.ModelSerializer):
     status = ChoiceField(choices=LOG_STATUS_CHOICES, default=0)
+    workout = WorkoutSerializer(read_only=True)
 
     class Meta:
         model = WorkoutLog
-        depth = 0
+        depth = 10
 
         fields = (
             'id',
